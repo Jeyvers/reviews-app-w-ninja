ccd9f7490e0bed7b8c4596c9ac909ee1b8378cd1
Jeyi Adole <meetjeyvers@gmail.com>
2023-08-08 15:26:31 +0100

    was working on second way to fetch fonts but deprecated

---
+++ app/_layout.js
@@ -0,0 +1,30 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    NunitoBold: require("../assets/fonts/Nunito/Nunito-Bold.ttf"),
+    NunitoSemiBold: require("../assets/fonts/Nunito/Nunito-SemiBold.ttf"),
+    NunitoMedium: require("../assets/fonts/Nunito/Nunito-Medium.ttf"),
+    NunitoRegular: require("../assets/fonts/Nunito/Nunito-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      // We only want to show our homepage if the fonts have loaded
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack onLayout={onLayoutRootView} />;
+};
+
+export default Layout;
--- app/index.js
+++ app/index.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
+import {} from "expo";
+import * as Font from "expo-font";
+
+const getFonts = () =>
+  Font.loadAsync({
+    NunitoRegular: require("../assets/fonts/Nunito/Nunito-Regular.ttf"),
+    NunitoBold: require("../assets/fonts/Nunito/Nunito-Bold.ttf"),
+  });
 
 export default function Home() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Home</Text>
