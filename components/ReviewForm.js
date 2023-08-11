e8aaecb1df76b5ecfb2d2c52c9552c2eabc26879
Jeyi Adole <meetjeyvers@gmail.com>
2023-08-11 12:58:51 +0100

    cleanups

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import React from "react";
 import { globalStyles } from "../styles/global";
 import { Formik } from "formik";
--- shared/Header.js
+++ shared/Header.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, ImageBackground } from "react-native";
+import { Text, Image, ImageBackground } from "react-native";
 import React from "react";
 import { StyleSheet } from "react-native";
 
